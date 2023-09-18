879b40c9e543368794d90643f678796bfcba5154
orizada <orizada@gmail.com>
2023-09-18 18:04:02 +0300

    modified:   AES/boxes.hpp 	added mixColumn to:   AES/utils.cpp

--- AES/utils.cpp
+++ AES/utils.cpp
@@ -14,15 +14,17 @@ void printState(unsigned char state[][4])
     }
 
 }
-/*
+
 void subBytes(unsigned char state[][4])
 {
-    for (int i=0;i<16;i++)
+    for (int i=0;i<4;i++)
     {
-        state[i] = sBox[state[i]];
+        for (int j=0;j<4;j++)
+        {
+            state[i][j] = sBox[state[i][j]/16][state[i][j]%16];
+        }
     }
 }
-*/
 
 void shiftRow(unsigned char state[][4])
 {
@@ -46,38 +48,39 @@ unsigned char if_two(unsigned char to_mul){ //calculates the num if it needs to 
         unsigned char temp = rest*2;
         return temp^0x1B; 
     }
-    
-    return to_mul*0x2;
-        
+    return to_mul*0x2;   
 }
 
-unsigned char calculate_num(unsigned char mul, unsigned char to_mul){ //gets a two numbers to be multiplied and returns the result
-    if(mul == 0x1){
+unsigned char calcNum(unsigned char mul, unsigned char to_mul){ //gets a two numbers to be multiplied and returns the result
+    if(mul == 0x1)
         return to_mul;
-    }
-    if(mul == 0x2){
+    if(mul == 0x2)
         return if_two(to_mul);
-    }
     return if_two(to_mul)^to_mul;
-
-    
-
 }
+
 
 void mixColumn(unsigned char state[][4])
 {
-    int i =0;
+    unsigned char tmp[4][4] = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+    for (int i=0;i<4;i++)
+    {
+        for (int j=0;j<4;j++)
+        {
+            for (int k=0;k<4;k++)
+            {
+                tmp[j][i] ^= calcNum(mixMatrix[j][k],state[k][i]);
+            }
+        }
+    }
+    state = tmp;
+    printState(state);
 }
 
 int main()
 {   
-    /*
-    unsigned char init_state[4][4] = {{0x01,0x02,0x03,0x04},{0x05,0x06,0x07,0x08},{0x09,0xA,0xB,0xC},{0xD,0xE,0xF,0x10}};
-    shiftRow(init_state);
-    printState(init_state);
-    */
-    unsigned char a = 0x4C;
-    unsigned char result = calculate_num(0x3,a);
-    std::cout<<(int)result;
+    
+    unsigned char init_state[4][4] = {{0x87,0xf2,0x4d,0x97},{0x6e,0x4c,0x90,0xec},{0x46,0xe7,0x4a,0xc3},{0xa6,0x8c,0xd8,0x95}};
+    mixColumn(init_state);
     return 0;
 }
