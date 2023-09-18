89e82b088e9089c21482309620e9fc942882c160
imri <imrisiecohen@gmail.com>
2023-09-18 16:15:19 +0300

    changed shift_row to unsigned char

--- AES/utils.cpp
+++ AES/utils.cpp
@@ -1,13 +1,13 @@
 #include "boxes.hpp"
 #include <iostream>
 
-void printState(int state[][4])
+void printState(unsigned char state[][4])
 {
     for (int i=0;i<4;i++)
     {
         for (int j=0;j<4;j++)
         {
-            std::cout<<state[i][j]<<' ';
+            std::cout<<(int)state[i][j]<<' ';
         }
         std::cout<<"\n";
     }
@@ -23,9 +23,9 @@ void subBytes(unsigned char state[][4])
 }
 */
 
-void shiftRow(int state[][4])
+void shiftRow(unsigned char state[][4])
 {
-    int tmp;
+    unsigned char tmp = 0x00; //starting value
     for (int i=1;i<4;i++) // loop over row
     {
         for (int j=0;j<i;j++) // how many times shift left
@@ -37,14 +37,17 @@ void shiftRow(int state[][4])
         }
     }
 }
-void mixColumn(int state[][4])
+
+
+
+void mixColumn(unsigned char state[][4])
 {
 
 }
 
 int main()
 {
-    int init_state[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    unsigned char init_state[4][4] = {{0x01,0x02,0x03,0x04},{0x05,0x06,0x07,0x08},{0x09,0xA,0xB,0xC},{0xD,0xE,0xF,0x10}};
     shiftRow(init_state);
     printState(init_state);
     return 0;
