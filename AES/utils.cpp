59e00da0e28f78eb9c61cb919447991c313fb4f4
imri <imrisiecohen@gmail.com>
2023-09-18 16:56:31 +0300

    added calculate num function

--- AES/utils.cpp
+++ AES/utils.cpp
@@ -1,5 +1,6 @@
 #include "boxes.hpp"
 #include <iostream>
+#include <vector>
 
 void printState(unsigned char state[][4])
 {
@@ -38,17 +39,45 @@ void shiftRow(unsigned char state[][4])
     }
 }
 
+unsigned char if_two(unsigned char to_mul){ //calculates the num if it needs to be multiplied by 2
+    
+    if(to_mul>=128){
+        unsigned char rest = to_mul-128;     
+        unsigned char temp = rest*2;
+        return temp^0x1B; 
+    }
+    
+    return to_mul*0x2;
+        
+}
 
+unsigned char calculate_num(unsigned char mul, unsigned char to_mul){ //gets a two numbers to be multiplied and returns the result
+    if(mul == 0x1){
+        return to_mul;
+    }
+    if(mul == 0x2){
+        return if_two(to_mul);
+    }
+    return if_two(to_mul)^to_mul;
 
-void mixColumn(unsigned char state[][4])
-{
+    
 
 }
 
-int main()
+void mixColumn(unsigned char state[][4])
 {
+    int i =0;
+}
+
+int main()
+{   
+    /*
     unsigned char init_state[4][4] = {{0x01,0x02,0x03,0x04},{0x05,0x06,0x07,0x08},{0x09,0xA,0xB,0xC},{0xD,0xE,0xF,0x10}};
     shiftRow(init_state);
     printState(init_state);
+    */
+    unsigned char a = 0x4C;
+    unsigned char result = calculate_num(0x3,a);
+    std::cout<<(int)result;
     return 0;
 }
